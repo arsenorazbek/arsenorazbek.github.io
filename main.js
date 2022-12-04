53be74fa96016fe7cccc8143bde09bb859fb84b9
Arsen Orazbek <111432128+arsenorazbek@users.noreply.github.com>
2022-12-04 19:10:45 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+/*Menu*/
 const navMenu = document.getElementById("nav-menu"),
       navToggle = document.getElementById("nav-toggle"),
       navClose = document.getElementById("nav-close")
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+
 if(navToggle){
     navToggle.addEventListener("click", () =>{
         navMenu.classList.add("show-menu")
@@ -13,8 +12,6 @@ if(navToggle){
 
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 if(navClose){
     navClose.addEventListener("click", () =>{
         navMenu.classList.remove("show-menu")
@@ -23,7 +20,7 @@ if(navClose){
 
 }
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/*remove menu in mb*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
@@ -33,7 +30,7 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== ACCORDION SKILLS ====================*/
+/*skills*/
 const skillsContent = document.getElementsByClassName('skills__content'),
       skillsHeader =document.querySelectorAll('.skills__header')
 function toggleSkills(){
@@ -50,7 +47,7 @@ skillsHeader.forEach((el)=>{
     el.addEventListener('click', toggleSkills)
 })
 
-/*==================== QUALIFICATION TABS ====================*/
+/*qualification*/
 const tabs = document.querySelectorAll('[data-target'),
       tabContents = document.querySelectorAll('[data-content')
 tabs.forEach(tab =>{
@@ -69,7 +66,7 @@ tabs.forEach(tab =>{
     })
 })
 
-/*==================== SERVICES MODAL ====================*/
+/*services*/
 const modalViews = document.querySelectorAll('.services__modal'),
       modalBtns = document.querySelectorAll('.services__button'),
       modalCloses = document.querySelectorAll('.services__modal-close')
@@ -92,7 +89,7 @@ modalCloses.forEach((modalClose) =>{
     })
 })
 
-/*==================== PORTFOLIO SWIPER  ====================*/
+/*achivements*/
 let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
@@ -106,7 +103,7 @@ let swiperPortfolio = new Swiper(".portfolio__container", {
     },
   });
 
-/*==================== TESTIMONIAL ====================*/
+/*extra*/
 let swiperTestimonial = new Swiper(".testimonial_container", {
     loop: true,
     grabCursor:true,
@@ -124,7 +121,7 @@ let swiperTestimonial = new Swiper(".testimonial_container", {
     }
   });
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/*scroll*/
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -144,48 +141,41 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*light/dark mode*/ 
 function scrollHeader(){
     const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*==================== SHOW SCROLL UP ====================*/ 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    
     if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
-/*==================== DARK LIGHT THEME ====================*/
+/*ligh/dark2*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-brightness'
 
-// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
+
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
-// We validate if the user previously chose a topic
+
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
-// Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 }) 
